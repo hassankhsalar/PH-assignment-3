3239cf1169b535ffeba0d48df0f3bb04031ed9f2
hassankhsalar <khsalarhassan@gmail.com>
2024-08-23 19:36:46 +0600

    3pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,18 @@ module.exports = {
       extend: {},
     },
     plugins: [],
+
+    theme: {
+      screens: {
+        'sm': '576px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    }
+
   }
